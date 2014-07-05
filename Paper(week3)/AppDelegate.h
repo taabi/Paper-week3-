7976c81b591106b2fb38d9301212b4a2fe6caeb5
taabi <taabi@taabi-macbookpro.roam.corp.google.com>
2014-07-05 12:01:24 -0700

    Initial Commit

---
+++ Paper(week3)/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paper(week3)
+//
+//  Created by taabi on 7/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
